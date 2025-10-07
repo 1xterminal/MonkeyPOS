e4a9b1231b5da333c6a83a7c4f88f16952e80ff4
1xterminal <jejevier1337@gmail.com>
2025-10-07 13:56:31 +0700

    refactor: header is now a component

---
+++ public/js/components/header.js
@@ -0,0 +1,47 @@
+function createHeader(root, userData) {
+  const header = $("<div>", {
+    "class": "header-actions"
+  }).append([
+    $("<div>", {
+      "class": "user-profile"
+    }).append([
+      $("<span>", {
+        "class": "material-symbols-outlined",
+        text: "person"
+      }),
+      $("<span>", {
+        text: `Hello, ${userData ? userData.name : "Guest"}`
+      })
+    ]),
+    $("<a>", {
+      "class": "logout-btn",
+      "href": "#"
+    }).append(
+      $("<span>", {
+        "class": "material-symbols-outlined",
+        text: "logout"
+      })
+    )
+  ]);
+
+  root.replaceWith(header);
+
+  // Logout handler
+  $(".logout-btn").on("click", (e) => {
+    e.preventDefault();
+    sessionStorage.removeItem("currentUser");
+    window.location.href = "../login/login.html";
+  });
+}
+
+$(document).ready(() => {
+  const currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
+
+  if (!currentUser) {
+    window.location.href = "../login/login.html";
+    return;
+  }
+
+  // Render header ke elemen .header-actions lama
+  createHeader($(".header-actions"), currentUser);
+});
